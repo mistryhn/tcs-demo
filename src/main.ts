3e12316730394252b9547901737f172750b6800c
hnm <mistrhnm@gmail.com>
2026-07-10 16:00:26 +0530

    Feat: updated with guards

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthGuard } from './guards/auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalGuards(new AuthGuard());
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -3,7 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: (error?: Error | any) => void) {
-    console.log(`${req.method} ${req.url}`);
+    console.log(`${req.method}--${req.url}`);
     next();
   }
 }
